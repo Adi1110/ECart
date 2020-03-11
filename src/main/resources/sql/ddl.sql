de0f4348911e2dc3867f20e5e0c024be85dc0c49
Aditya <ranjan.aditya03@gmail.com>
2020-03-11 17:12:18 +0530

    Final commit

---
+++ src/main/resources/sql/ddl.sql
@@ -0,0 +1,6 @@
+CREATE TABLE product (
+  id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name varchar(45) DEFAULT NULL,
+  price decimal(10,0) DEFAULT NULL,
+  quantity int(45) DEFAULT NULL,
+  description varchar(450) DEFAULT NULL);
